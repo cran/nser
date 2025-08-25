3d3fde454675bad135c7c3cd0a28e8584cdf8c8f
Nandan Patil <tryanother609@gmail.com>
2025-08-25 08:30:02 +0000

    version 1.5.6

--- R/fobhav1.R
+++ R/fobhav1.R
@@ -1,27 +1,26 @@
-#' @name bhav1
-#' @aliases bhav1
-#' @title Bhavcopy from NSE upto 05 July 2024
+#' @name fobhav1
+#' @aliases fobhav1
+#' @title Futures and Options Bhavcopy from NSE
 #'
 #' @param x numeric date format
 #'
-#' @note The date should be strictly numerical and mentioned in quotation mark (refer examples). `bhav1` can be used to download bhavcopy from 1 Jan 2016 to 05 July 2024.
-#' @return Bhavcopy for the given date.
+#' @note The date should be strictly numerical and mentioned in quotation mark. `fobhav` can be used to download bhavcopy from 1 Jan 2016 onwards.
+#' @return F&O Bhavcopy for the given date.
 #' @author Nandan L. Patil \email{tryanother609@@gmail.com}
-#' @details Gets Bhavcopy from NSE from 1 Jan 2016 to 05 July 2024.
+#' @details Gets Futures and Options Bhavcopy from NSE for the given date.
 #' @source <https://www.nseindia.com/all-reports>
-#' @seealso \code{\link[nser]{bhav}}\code{\link[nser]{bhavtoday}}
+#'
+#' @seealso \code{\link[nser]{bhavpr}}\code{\link[nser]{bhavtoday}}
 #'
 #' @importFrom utils download.file read.csv unzip
 #' @importFrom curl has_internet
+#'
 #' @export
 #'
-#' @examples \donttest{
-#' #Download Bhavcopy from NSE
-#' report = bhav1("01072021") # Download bhavcopy for 01 July 2021
+#' @examples report = fobhav1("01072021") # Download F&O bhavcopy for 01 July 2021
 #'
-#' }
-bhav1 = function(x){
-  # First check internet connection
+fobhav1 = function(x){
+  # check internet connection
   if (!curl::has_internet()) {
     message("No internet connection.")
     return(invisible(NULL))
@@ -61,22 +60,15 @@ bhav1 = function(x){
       mt = "DEC"
     }
 
-    baseurl = "https://archives.nseindia.com/content/historical/EQUITIES/"
-
+    baseurl = "https://archives.nseindia.com/content/historical/DERIVATIVES/"
     end = ".csv.zip"
-    bhavurl = paste0(baseurl, yr, "/", mt, "/cm", dy, mt, yr, "bhav", end)
-    zipname = paste0("cm", dy, mt, yr, "bhav", ".csv")
+    bhavurl = paste0(baseurl, yr, "/", mt, "/fo", dy, mt, yr, "bhav", end)
+    zipname = paste0("fo", dy, mt, yr, "bhav", ".csv")
 
-    bhav11 = function(x){
-      temp <- tempfile()
-      download.file(bhavurl, temp)
-      file = read.csv(unz(temp, filename = zipname))
-      unlink(temp)
-      return(file)
-    }
-
-      df = bhav11()
-
-    return(df)
+    temp <- tempfile()
+    download.file(bhavurl, temp)
+    file <-  read.csv(unz(temp, filename = zipname))
+    unlink(temp)
+    return(file)
   }
 }
